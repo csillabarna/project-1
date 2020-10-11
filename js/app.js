9f923dd8018df7193f84372d4bebba4647152964
Csilla Barna <barnacsilla89@gmail.com>
2020-10-11 16:54:47 +0100

    refactor, clear function added

--- js/app.js
+++ js/app.js
@@ -6,32 +6,35 @@ const grid = document.querySelector('.grid')
 const width = 5
 const cells = []
 
-
-
+// create the grid
 for (let i = 0; i < width ** 2; i++) {
   const div = document.createElement('div')
   grid.appendChild(div)
   div.innerHTML = i //remove later
   cells.push(div)
 }
-
 // fill every cell with random colors
-
-
-start.addEventListener('click', () => {
+function colorFill() {
   cells.forEach((cell) => {
-
-    if (!cell.classList.contains('red', 'green', 'blue')) {
-      const colors = ['red', 'green', 'blue']
-      const randomColorIndex = Math.floor(Math.random() * colors.length)
-      const randomColor = colors[randomColorIndex]
-      cell.classList.add(randomColor)
-      // console.log(Array.from(cell.classList))
-    }
+    const colors = ['red', 'green', 'blue']
+    const randomColorIndex = Math.floor(Math.random() * colors.length)
+    const randomColor = colors[randomColorIndex]
+    cell.classList.add(randomColor)
+    // console.log(Array.from(cell.classList))
   })
+}
+// remove all classes from cell
+function clear() {
+  cells.forEach((cell) => {
+    // if ((cell.classList.contains('red')) || (cell.classList.contains('green')) || (cell.classList.contains('blue'))) {
+    cell.className = ''
+    // }
+  })
+}
 
-  //check 3 matches row(and remove it)
 
+//check 3 matches in rows
+function match3Row() {
   for (let i = 0; i < width ** 2; i++) {
 
     if ((i % width === width - 2) || (i % width === width - 1)) {
@@ -43,15 +46,18 @@ start.addEventListener('click', () => {
       // console.log(second)
       const third = cells[i + 2].classList[0]
       // console.log(third)
-      if (first === second && first === third) {
+      const match3R = first === second && first === third
+      if (match3R) {
         console.log(`starting from ${i} ${first} and ${second} and ${third} are the same in the row `)
 
       }
 
     }
   }
+}
 
-  //check 3 matches column 
+//check 3 matches in columns
+function match3Column() {
 
   for (let i = 0; i < width ** 2; i++) {
 
@@ -64,7 +70,8 @@ start.addEventListener('click', () => {
       // console.log(second)
       const third = cells[i + width * 2].classList[0]
       // console.log(third)
-      if (first === second && first === third) {
+      const match3C = first === second && first === third
+      if (match3C) {
         console.log(` starting from ${i}  ${first} and ${second} and ${third} are the same in the column `)
 
       }
@@ -72,15 +79,17 @@ start.addEventListener('click', () => {
     }
   }
 
+}
+// start button event listener
 
-
-
-
-
-
-
-
+start.addEventListener('click', () => {
+  colorFill()
+  match3Row()
+  match3Column()
 })
 
 
-
+// reset button event listener remove all classes 
+reset.addEventListener('click', () => {
+  clear()
+})
