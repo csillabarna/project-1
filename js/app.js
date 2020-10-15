6dd38715adb6f6c12827c85dab2617973dde0d08
Csilla Barna <barnacsilla89@gmail.com>
2020-10-15 14:13:23 +0100

    localstorage added - high scores

--- js/app.js
+++ js/app.js
@@ -7,16 +7,17 @@ const cells = []
 let selected = -1
 const colors = ['red', 'green', 'blue', 'pink']
 const crush3Points = 3
-let startTime = 15
+let startTime = 10
 const targetTime = 60
+let scoresTable = []
+const scoresList = document.querySelector('ol')
+let newName = ''
 
 // create the grid
 for (let i = 0; i < width ** 2; i++) {
   const div = document.createElement('div')
   grid.appendChild(div)
   div.setAttribute('id', i)
-
-  div.innerHTML = i //remove later
   cells.push(div)
 }
 // fill every cell with random colors
@@ -25,16 +26,12 @@ function randomColorFill(array) {
     const randomColorIndex = Math.floor(Math.random() * colors.length)
     const randomColor = colors[randomColorIndex]
     cell.classList.add(randomColor)
-
-    // console.log(`random fill on ${i} ${randomColor}`)
   })
 }
 // remove all classes from cell
 function clear() {
   cells.forEach((cell) => {
-    // if ((cell.classList.contains('red')) || (cell.classList.contains('green')) || (cell.classList.contains('blue'))) {
     cell.className = ''
-    // }
   })
 }
 
@@ -54,14 +51,16 @@ function checkCrushFill(countPoints) {
     match3AndFillC(countPoints)
   }
 }
-
-
+// highest()
 // start button event listener
 start.addEventListener('click', () => {
-  clear()
+  newName = prompt('Enter your name')
+
   randomColorFill(cells)
   checkCrushFill(false)
+
   disableBtn()
+
   let intervalId = 0
   if (intervalId) {
     return
@@ -69,22 +68,55 @@ start.addEventListener('click', () => {
   intervalId = setInterval(() => {
     if (startTime === 0) {
       clearInterval(intervalId)
-      alert(`time is up you have ${points.innerHTML} points`)
+
+      alert(`Hey ${newName} your time is up you have ${points.innerHTML} points`)
+      highScores()
       points.innerHTML = 0
+      clear()
       enableBtn()
       startTime = targetTime
+
     } else {
-      // Decrease my timer every second
       startTime--
       console.log('running timer')
       time.innerHTML = startTime
     }
   }, 1000)
 })
+cells.forEach((cell) => {
+  cell.addEventListener('click', () => {
+    if (selected >= 0) {
+      const firstId = selected
+      // console.log('first selected ID is:', firstId)
+      const firstColor = cells[firstId].classList[0]
+      const secondId = parseInt(cell.id)
+      const secondColor = cell.classList[0]
+      const firstCell = cells[firstId]
+      const secondCell = cell
 
+      if (neighbourCheck(firstId, secondId) && colorCheck(firstId, firstColor, secondId, secondColor)) {
+        firstCell.classList.remove(firstColor)
+        firstCell.classList.add(secondColor)
 
+        secondCell.classList.remove(secondColor)
+        secondCell.classList.add(firstColor)
+        checkCrushFill(true)
 
-
+      } else {
+        secondCell.classList.add('shake')
+        console.log('shake has been added')
+      }
+      selected = -1
+    } else {
+      selected = parseInt(cell.id)
+      console.log('Selected', selected)
+      if (document.querySelector('.shake')) {
+        document.querySelector('.shake').classList.remove('shake')
+      }
+    }
+  })
+})
+// neigbour cell check
 function neighbourCheck(firstId, secondId) {
   console.log('Running Neighbor check')
   return secondId === firstId + 1
@@ -92,23 +124,9 @@ function neighbourCheck(firstId, secondId) {
     || secondId === firstId + width
     || secondId === firstId - width
 }
-// let colorCheck = false
-// const arrayCheck = [[tt, t], [t, b], [b, bb], [ll, l], [l, r], [r, rr]]
-// let i = 0
-// while (i < arrayCheck.length && colorCheck === false) {
-//   if (arrayCheck[i][0] && arrayCheck[i][1] && arrayCheck[i][0].classList[1] === color && arrayCheck[i][1].classList[1] === color) {
-//     colorCheck = true
-//   } else {
-//     colorCheck = false
-//   }
-//   i++
-// }
-
-
+// check the color of the clicked ones and where is it coloumn or row,then put in the right order
 function colorCheck(firstId, firstColor, secondId, secondColor) {
-  console.log('Running Color check')
   const isRow = Math.abs(firstId - secondId) === 1
-
   if (isRow) {
     if (firstId > secondId) {
       return colorCheckRow(secondId, secondColor, firstId, firstColor)
@@ -125,15 +143,12 @@ function colorCheck(firstId, firstColor, secondId, secondColor) {
 }
 
 function colorCheckRow(leftId, leftColor, rightId, rightColor) {
-  console.log('row', leftId, leftColor, rightId, rightColor)
-
   const checkAgainstLeft = [
     [cells[rightId + 1], cells[rightId + 2]],
     [cells[rightId - width], cells[rightId - width * 2]],
     [cells[rightId + width], cells[rightId + width * 2]],
     [cells[rightId + width], cells[rightId - width]]
   ]
-
   const checkAgainstRight = [
     [cells[leftId - 1], cells[leftId - 2]],
     [cells[leftId - width], cells[leftId - width * 2]],
@@ -141,21 +156,16 @@ function colorCheckRow(leftId, leftColor, rightId, rightColor) {
     [cells[leftId + width], cells[leftId - width]]
   ]
   for (let i = 0; i < checkAgainstLeft.length; i++) {
-    // console.log(checkAgainstLeft[i][0], checkAgainstLeft[i][1], i, ' check against left colors')
     if (checkAgainstLeft[i][0] && checkAgainstLeft[i][0].classList[0] === leftColor
       && checkAgainstLeft[i][1] && checkAgainstLeft[i][1].classList[0] === leftColor) {
-
       return true
     }
-
   }
   for (let i = 0; i < checkAgainstRight.length; i++) {
-    // console.log(checkAgainstLeft[i][0], checkAgainstLeft[i][1], i, ' check against right colors')
     if (checkAgainstRight[i][0] && checkAgainstRight[i][0].classList[0] === rightColor
       && checkAgainstRight[i][1] && checkAgainstRight[i][1].classList[0] === rightColor) {
       return true
     }
-
   }
   return false
 }
@@ -163,85 +173,35 @@ function colorCheckRow(leftId, leftColor, rightId, rightColor) {
 function colorCheckColumn(topId, topColor, bottomId, bottomColor) {
   console.log('column', topId, topColor, bottomId, bottomColor)
   const checkAgainstTop = [
-
     [cells[bottomId + width], cells[bottomId + width * 2]],
     [cells[bottomId + 1], cells[bottomId + 2]],
     [cells[bottomId - 1], cells[bottomId - 2]],
     [cells[bottomId - 1], cells[bottomId + 1]]
   ]
-
   const checkAgainstBottom = [
     [cells[topId - width], cells[topId - width * 2]],
     [cells[topId + 1], cells[topId + 2]],
     [cells[topId - 1], cells[topId - 2]],
     [cells[topId - 1], cells[topId + 1]]
   ]
-
   for (let i = 0; i < checkAgainstTop.length; i++) {
     if (checkAgainstTop[i][0] && checkAgainstTop[i][0].classList[0] === topColor
       && checkAgainstTop[i][1] && checkAgainstTop[i][1].classList[0] === topColor) {
       return true
     }
-
   }
   for (let i = 0; i < checkAgainstBottom.length; i++) {
     if (checkAgainstBottom[i][0] && checkAgainstBottom[i][0].classList[0] === bottomColor
       && checkAgainstBottom[i][1] && checkAgainstBottom[i][1].classList[0] === bottomColor) {
       return true
     }
-
   }
   return false
 }
 
-
-cells.forEach((cell) => {
-  cell.addEventListener('click', () => {
-
-    if (selected >= 0) {
-      const firstId = selected
-      console.log('first selected ID is:', firstId)
-      const firstColor = cells[firstId].classList[0]
-      console.log(firstColor)
-      const secondId = parseInt(cell.id)
-      console.log('second selected ID is:', secondId)
-      const secondColor = cell.classList[0]
-      console.log(secondColor)
-      const firstCell = cells[firstId]
-      const secondCell = cell
-
-      if (neighbourCheck(firstId, secondId) && colorCheck(firstId, firstColor, secondId, secondColor)) {
-        firstCell.classList.remove(firstColor)
-        firstCell.classList.add(secondColor)
-
-        secondCell.classList.remove(secondColor)
-        secondCell.classList.add(firstColor)
-        checkCrushFill(true)
-        console.log('Finished')
-      } else {
-        secondCell.classList.add('shake')
-        console.log('shake has been added')
-
-      }
-
-      selected = -1
-
-    } else {
-      selected = parseInt(cell.id)
-      console.log('Selected', selected)
-      if (document.querySelector('.shake')) {
-        document.querySelector('.shake').classList.remove('shake')
-      }
-    }
-
-  })
-})
-
 function match3AndFillR(countPoints) {
   console.log('Filling row matches')
-
   let hasRowMatch = true
-
   while (hasRowMatch) {
     for (let i = 0; i < width ** 2; i++) {
       if (i === (width ** 2) - 1) {
@@ -255,7 +215,6 @@ function match3AndFillR(countPoints) {
         const third = cells[i + 2].classList[0]
 
         const match3R = first === second && first === third
-
         if (match3R) {
           console.log(`starting from ${i} ${first} and ${second} and ${third} are the same in the row `)
           rowRemoveAndFill(i, countPoints)
@@ -266,10 +225,8 @@ function match3AndFillR(countPoints) {
     }
   }
 }
-
 function match3AndFillC(countPoints) {
   console.log('Filling column matches')
-
   let hasColumnMatch = true
 
   while (hasColumnMatch) {
@@ -285,13 +242,11 @@ function match3AndFillC(countPoints) {
         const third = cells[i + width * 2].classList[0]
 
         const match3C = first === second && first === third
-
         if (match3C) {
           console.log(` starting from ${i}  ${first} and ${second} and ${third} are the same in the column `)
           columnRemoveAndFill(i, countPoints)
           break
         }
-
       }
     }
   }
@@ -300,7 +255,6 @@ function match3AndFillC(countPoints) {
 function match3Row() {
   console.log('Checking row matches')
   for (let i = 0; i < width ** 2; i++) {
-
     if ((i % width === width - 2) || (i % width === width - 1)) {
       // console.log(`ignoring i ${i}`)
     } else {
@@ -312,7 +266,6 @@ function match3Row() {
         // console.log(`starting from ${i} ${first} and ${second} and ${third} are the same in the row `)
         return true
       }
-
     }
   }
 }
@@ -344,7 +297,6 @@ function rowRemoveAndFill(index, countPoints) {
     const second = cells[i + 1].classList[0]
     const third = cells[i + 2].classList[0]
     const arrayOfThree = [cells[i], cells[i + 1], cells[i + 2]]
-
     if (i < width) {
       console.log(i, first, second, third, 'in row')
       cells[i].classList.remove(first), cells[i + 1].classList.remove(second), cells[i + 2].classList.remove(third)
@@ -371,7 +323,6 @@ function columnRemoveAndFill(index, countPoints) {
     const third = cells[i + width * 2].classList[0]
     console.log(i, first, second, third, ' in column')
     const arrayOfThree = [cells[i], cells[i + width], cells[i + width * 2]]
-    // console.log(arrayOfThree, 'column')
 
     // 1. special case - starting from the first row
     if (i < width) {
@@ -385,7 +336,6 @@ function columnRemoveAndFill(index, countPoints) {
       // console.log(i, first, second, third, 'has been removed')
       const newThird = cells[i - width].classList[0]
       cells[i + width * 2].classList.add(newThird)
-      // console.log(newThird, ' added as a new third color')
       // remove color class from the cells from top and after assign a new random color
       cells[i - width].classList.remove(newThird)
       //we copyd and removed a color from cells[i] and the rest also been removed so need a new random color
@@ -398,12 +348,12 @@ function columnRemoveAndFill(index, countPoints) {
       const newSecond = cells[i - width * 2].classList[0]
       const newThird = cells[i - width].classList[0]
       cells[i + width].classList.add(newSecond), cells[i + width * 2].classList.add(newThird)
-      // console.log(newSecond, newThird, ' added as a new color')
       // remove color class from the cells from top and second row and after assign a new random color
       cells[i - width * 2].classList.remove(newSecond)
       cells[i - width].classList.remove(newThird)
       //  we copy the color from theese cells but they will need a new random color and also for the third cell (cell[i])
       randomColorFill([cells[i - width * 2], cells[i - width], cells[i]])
+
     } else {
       const newFirst = cells[i - width * 3].classList[0]
       const newSecond = cells[i - width * 2].classList[0]
@@ -411,7 +361,43 @@ function columnRemoveAndFill(index, countPoints) {
       cells[i].classList.remove(first), cells[i + width].classList.remove(second), cells[i + width * 2].classList.remove(third)
       cells[i].classList.add(newFirst), cells[i + width].classList.add(newSecond), cells[i + width * 2].classList.add(newThird)
     }
-
+  }
+}
+// high Score table
+function highScores() {
+  if (localStorage) {
+    scoresTable = JSON.parse(localStorage.getItem('scores')) || []
+    orderAndDisplayScores()
   }
 
+
+  const player = { name: newName, score: Number(points.innerHTML) }
+  scoresTable.push(player)
+  if (localStorage) {
+    localStorage.setItem('scores', JSON.stringify(scoresTable))
+  }
+  orderAndDisplayScores()
+
 }
+
+function orderAndDisplayScores() {
+  const array = scoresTable
+    .sort((playerA, playerB) => playerB.score - playerA.score)
+    .map(player => {
+      return `<li>
+        ${player.name} has ${player.score} points
+      </li>`
+    })
+  scoresList.innerHTML = array.join('')
+}
+
+// function highest() {
+//   const array = scoresTable
+//     .sort((playerA, playerB) => playerB.score - playerA.score)
+//     .map(player => {
+//       return `<li>
+//         ${player.name} has ${player.score} points
+//       </li>`
+//     })
+//   scoresList.innerHTML = array[0]
+// }
